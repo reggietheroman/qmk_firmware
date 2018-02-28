a5fe07164512801be2dad0b7a229df7697a7e39b
Reggie <reggietheroman@gmail.com>
2018-02-28 18:39:31 +0800

    Udate: removed colemak layer, added arrows to adjust layer, added macro layer that is a numpad and basic windows shortcuts.

--- keyboards/lets_split/keymaps/hazzula/keymap.c
+++ keyboards/lets_split/keymaps/hazzula/keymap.c
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |    |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------| LCTL(KC_Y)
- * |Adjust| Ctrl | GUI  | Alt  |Lower | Bksp |    |Space |Raise | Left | Down |  Up  |Right |
+ * |Adjust| Ctrl | GUI  | Alt  |Lower | Bksp |    |Space |Raise |Adjust| Down |  Up  |Right |
  * `-----------------------------------------'    `-----------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC, KC_SPC,  RAISE,   ADJUST,  KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Dvorak
@@ -68,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  * | Shift|   ;  |   Q  |   J  |   K  |   X  |    |   B  |   M  |   W  |   V  |   Z  |Enter |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
- * |Adjust| Ctrl | GUI  | Alt  |Lower | Bksp |    |Space |Raise | Left | Down |  Up  |Right |
+ * |Adjust| Ctrl | GUI  | Alt  |Lower | Bksp |    |Space |Raise |Adjust| Down |  Up  |Right |
  * `-----------------------------------------'    `-----------------------------------------'
  */
 [_DVORAK] = KEYMAP( \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,   KC_BSPC, \
+  KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,   KC_SLSH, \
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,   KC_ENT , \
+  ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC, KC_SPC,  RAISE,   ADJUST,  KC_DOWN, KC_UP,  KC_RGHT \
 ),
 
 /* Macros (also has a numpad on the RH side)
@@ -128,26 +128,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, KC_TAB, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_D,    KC_NUHS, KC_NUBS, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, KC_TAB,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------.    ,-----------------------------------------.
- * |      | Reset|      |AGnorm|AGswap|      |    |Macros|Qwerty|Dvorak|      |      |  Del |
+ * |      | Reset|      |AGnorm|AGswap|      |    |Macros|      |  Up  |      |      |  Del |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|      |    | Left | Down |  Up  |Right |      |      |
+ * |      |      |      |Aud on|Audoff|      |    |Dvorak| Left | Down |Right |      |      |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
- * |      |      | CSPD | CSND |      |      |    |      |      |      |      |      |      |
+ * |      |      | CSPD | CSND |      |      |    |Qwerty|      |      |      |      |      |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |    |      |      |      |      |      |      |
  * `-----------------------------------------'    `-----------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
-  _______, RESET,   _______, AG_NORM, AG_SWAP, _______, TG(_MACROS), QWERTY,  DVORAK,    _______,  _______, KC_DEL,  \
-  _______, _______, _______, AU_ON,   AU_OFF,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
-  _______, _______, CSPD,    CSND,    _______, _______, _______, _______, _______, _______,  _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______ \
+  _______, RESET,   _______, AG_NORM, AG_SWAP, _______, TG(_MACROS), _______, KC_UP,   _______,  _______, KC_DEL,  \
+  _______, _______, _______, AU_ON,   AU_OFF,  _______, DVORAK,      KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, \
+  _______, _______, CSPD,    CSND,    _______, _______, QWERTY,      _______, _______, _______,  _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______,  _______, _______ \
 )
 
 
